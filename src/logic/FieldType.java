0e2e40010ba9c40db5012255104bda902ca587ec
Lilly Schumann <lillyschumann@googlemail.com>
2020-06-18 19:29:02 +0200

    angefangen mit funktionen zur punkteberechnung

---
+++ src/logic/FieldType.java
@@ -0,0 +1,6 @@
+package logic;
+
+public enum FieldType {
+    Einser, Zweier, Dreier, Vierer, Fuenfer, Sechser,
+    Dreierpasch, Viererpasch, FullHouse, KleineStraße, GroßeStraße, Kniffel, Chance
+}

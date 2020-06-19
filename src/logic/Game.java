25be9ba7e9b23d6578f88fe5f742f923ebaca0f9
Exoucia Mukubay <exoucia.m@web.de>
2020-06-19 17:27:09 +0200

    test

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -3,7 +3,11 @@ package logic;
 import javax.swing.*;
 
 public class Game extends JFrame {
-    private Dice[] dices = new Dice[4];
+	/** 
+	 * vorher stand newDice [4]
+	 * @author prett
+	 */
+    private Dice[] dices = new Dice[5];
     private Field[] fields = new Field[13];
 
 }
--- src/logic/GameController.java
+++ src/logic/GameController.java
@@ -1,14 +1,52 @@
 package logic;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.*;
+
 public class GameController implements ActionListener {
 
+	/**
+	 * Ich habe den Button "Würfeln" implementiert
+	 * @author prett
+	 * 
+	 */
+	private JButton clickRoll;
+	
+	
+	
     private Game game;
+    
+    /**
+     * Konstruktor
+     * @author prett
+     */
+    
+    public GameController() {
+    	
+    	/**
+    	 * "Würfeln" als ActionListener eingetragen
+    	 * @author prett
+    	 */
+    	clickRoll.addActionListener(this);
+    	
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+    	
+    	
+    	/**
+    	 * Wenn ich würfeln geklickt habe passiert.....
+    	 * @author prett
+    	 */
+    	if(e.getSource() == this.clickRoll) {
+    		
+    	}
 
     }
+
+	
 }

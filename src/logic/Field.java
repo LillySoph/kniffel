0fd432e54e076789ddc4201956caf189fc32b557
Exoucia Mukubay <exoucia.m@web.de>
2020-06-22 18:32:01 +0200

    calculate Large und SmallStraight müssen noch bearbeitet werden

--- src/logic/Field.java
+++ src/logic/Field.java
@@ -4,161 +4,283 @@ import javax.swing.*;
 
 public class Field extends JButton {
 
-    private int points;
-    private FieldType fieldType;
+	//TODO Small Straight &  LargeStraight fehlen noch @author prett
+	
+	private int points;
+	private FieldType fieldType;
 
-    // creates empty field button of given type
-    public Field(FieldType fieldType) {
-        this.fieldType = fieldType;
-        // field is empty
-        this.points = -1;
-    }
+	// creates empty field button of given type
+	public Field(FieldType fieldType) {
+		this.fieldType = fieldType;
+		// field is empty
+		this.points = -1;
+	}
 
-    public void resetPoints() {
-    	this.points = -1;
-    }
-    
-    public int getPoints() { return this.points; }
+	public void resetPoints() {
+		this.points = -1;
+	}
 
-    // calculates points depending on given dices & field type and updates button text
-    public void updatePoints(Dice[] dices) {
+	public int getPoints() {
+		return this.points;
+	}
 
-        // field was zero before or is now being set to zero
-        if(points >= 0) {
-            this.points = 0;
-            this.setText(toString());
-            return;
-        }
-        // otherwise calculate points
-        switch(this.fieldType) {
-            case Aces:
-                this.points = calculatePointsForFirstBlock(dices, 1);
-                break;
-            case Twos:
-                this.points = calculatePointsForFirstBlock(dices, 2);
-                break;
-            case Threes:
-                this.points = calculatePointsForFirstBlock(dices, 3);
-                break;
-            case Fours:
-                this.points = calculatePointsForFirstBlock(dices, 4);
-                break;
-            case Fives:
-                this.points = calculatePointsForFirstBlock(dices, 5);
-                break;
-            case Sixes:
-                this.points = calculatePointsForFirstBlock(dices, 6);
-                break;
-            case ThreeOfOneKind:
-                this.points = calculatePointsforNOfAKind(dices, 3);
-                break;
-            case FourOfOneKind:
-                this.points = calculatePointsforNOfAKind(dices, 4);
-                break;
-            case Kniffel:
-                this.points = calculatePointsforNOfAKind(dices, 5);
-                break;
-            case FullHouse:
-                this.points = calculatePointsforFullHouse(dices);
-                break;
-            case SmallStraight:
-                this.points = calculatePointsforStraight(dices, 4);
-                break;
-            case LargeStraight:
-                this.points = calculatePointsforStraight(dices, 5);
-                break;
-            default:
-                this.points = calculatePointsforChance(dices);
-        }
+	// calculates points depending on given dices & field type and updates button
+	// text
+	public void updatePoints(Dice[] dices) {
 
-        // update button text
-        this.setText(toString());
-    }
+		// field was zero before or is now being set to zero
+		if (points >= 0) {
+			this.points = 0;
+			this.setText(toString());
+			return;
+		}
+		// otherwise calculate points
+		switch (this.fieldType) {
+		case Aces:
+			this.points = calculatePointsForFirstBlock(dices, 1);
+			break;
+		case Twos:
+			this.points = calculatePointsForFirstBlock(dices, 2);
+			break;
+		case Threes:
+			this.points = calculatePointsForFirstBlock(dices, 3);
+			break;
+		case Fours:
+			this.points = calculatePointsForFirstBlock(dices, 4);
+			break;
+		case Fives:
+			this.points = calculatePointsForFirstBlock(dices, 5);
+			break;
+		case Sixes:
+			this.points = calculatePointsForFirstBlock(dices, 6);
+			break;
+		case ThreeOfOneKind:
+			this.points = calculatePointsOfAKind(dices, 3);
+			break;
+		case FourOfOneKind:
+			this.points = calculatePointsOfAKind(dices, 4);
+			break;
+		case Kniffel:
+			this.points = calculatePointsOfAKind(dices, 5);
+			break;
+		case FullHouse:
+			this.points = calculatePointsForFullHouse(dices);
+			break;
+		case SmallStraight:
+			this.points = calculatePointsForStraight(dices, 4);
+			break;
+		case LargeStraight:
+			this.points = calculatePointsForStraight(dices, 5);
+			break;
+		default:
+			this.points = calculatePointsForChance(dices);
+		}
 
-    public String toString() { return this.points + "";}
+		// update button text
+		this.setText(toString());
+	}
 
-    // calculates and returns points for Aces, Twos, Threes, Fours, Fives and Sixes
-    private int calculatePointsForFirstBlock(Dice[] dices, int n) {
-        int result = 0;
-        for(int i=0; i<dices.length; i++) {
-            if(dices[i].getValue() == n)
-                result += n;
-        }
-        return result;
-    }
+	public String toString() {
+		return this.points + "";
+	}
 
-    // calculates and returns points for ThreeOfAKind, FourOfAKind and Kniffel
-    private int calculatePointsforNOfAKind(Dice[] dices, int n) {
-        int counter = 0;
-        int sum = 0;
-        for(int i=0; i<dices.length; i++) {
-            if(dices[i].getValue() == n)
-                counter++;
-            sum += dices[i].getValue();
-        }
-        if(counter < n)
-            return 0;
-        else if(this.fieldType == FieldType.Kniffel)
-            return 50;
-        else
-            return sum;
-    }
+	/**
+	 * calculates and returns points for Aces, Twos, Threes, Fours, Fives and Sixes
+	 * 
+	 * @param dices
+	 * @param n     , number of dice value regarding to field type
+	 * @return sum of dice with appropriate field type
+	 */
+	private int calculatePointsForFirstBlock(Dice[] dices, int n) {
+		int result = 0;
+		for (int i = 0; i < dices.length; i++) {
+			if (dices[i].getValue() == n)
+				result += n;
+		}
+		return result;
+	}
 
-    // calculates and returns points for FullHouse
-    private int calculatePointsforFullHouse(Dice[] dices) {
-        boolean found3OfAKind = false;
-        int counter3 = 0;
-        int num3 = 1;
-        while(!found3OfAKind && num3 <= 6) {
-            for (int i = 0; i < dices.length; i++) {
-                if (dices[i].getValue() == num3)
-                    counter3++;
-            }
-            if(counter3 == 3)
-                found3OfAKind = true;
-        }
-        boolean found4OfAKind = false;
-        int counter4 = 0;
-        int num4 = 1;
-        while(!found4OfAKind && num4 <= 6) {
-            for (int i = 0; i < dices.length; i++) {
-                if (dices[i].getValue() == num4)
-                    counter4++;
-            }
-            if (counter4 == 3)
-                found4OfAKind = true;
-        }
-        if(found3OfAKind == true && found4OfAKind == true && num3 != num4)
-            return 25;
-        else
-            return 0;
-    }
+	/**
+	 * Uses one dice to compare its value with other four dice values
+	 * 
+	 * @param dices
+	 * @param nInARow , number of numbers in a row
+	 * @return points for ThreeOfAKind(sum of all dice), FourOfAKind (sum of all
+	 *         dice) and Kniffel(50)
+	 */
 
-    // calculates and returns points for SmalLStraight and LargeStraight
-    private int calculatePointsforStraight(Dice[] dices, int nInARow) {
-        int points = (nInARow - 1) * 10;
-        int start = 1;
-        boolean straight = false;
-        while(!straight || start == 7 - nInARow) {
-            for (int i = 0; i < dices.length; i++) {
-                if (dices[i].getValue() == start)
-                    start++;
-            }
-            if (start >= nInARow)
-                straight = true;
-        }
-        if(!straight)
-            return 0;
-        else
-            return points;
-    }
+	private int calculatePointsOfAKind(Dice[] dices, int nInARow) {
+		int counter = 1, sum = 0, i = 0;
+		boolean foundNumbersInARow = false;
 
-    // calculates and returns points for Chance
-    private int calculatePointsforChance(Dice[] dices) {
-        int result = 0;
-        for(int i=0; i<dices.length; i++) {
-            result += dices[i].getValue();
-        }
-        return result;
-    }
+		while (!foundNumbersInARow && i < dices.length) {
+
+			for (int j = i + 1; j <= dices.length - 1; j++) {
+				if (dices[i].getValue() == dices[j].getValue()) {
+					counter++;
+				}
+				if (counter == nInARow) {
+					foundNumbersInARow = true;
+				}
+			}
+			// start with next index of dice
+			counter = 1;
+			i++;
+		}
+
+		// calculates points
+		if (foundNumbersInARow) {
+			if (this.fieldType == FieldType.Kniffel) {
+				return 50;
+			} else {
+				for (i = 0; i < dices.length; i++) {
+					sum += dices[i].getValue();
+				}
+				return sum;
+			}
+		} else {
+			// condition failed
+			return 0;
+		}
+
+	}
+
+	// calculates and returns points for FullHouse
+	/*
+	 * private int calculatePointsForFullHouse(Dice[] dices) { boolean found3OfAKind
+	 * = false; int counter3 = 0; int num3 = 1; while (!found3OfAKind && num3 <= 6)
+	 * { for (int i = 0; i < dices.length; i++) { if (dices[i].getValue() == num3)
+	 * counter3++; } if (counter3 == 3) found3OfAKind = true; } boolean
+	 * found4OfAKind = false; int counter4 = 0; int num4 = 1; while (!found4OfAKind
+	 * && num4 <= 6) { for (int i = 0; i < dices.length; i++) { if
+	 * (dices[i].getValue() == num4) counter4++; } if (counter4 == 3) found4OfAKind
+	 * = true; } if (found3OfAKind == true && found4OfAKind == true && num3 != num4)
+	 * return 25; else return 0; }
+	 */
+
+	/**
+	 * Uses one dice to compare its value with other four dice values
+	 * 
+	 * @param dices
+	 * @return 25 points if three of one number and two of another number exist in a
+	 *         row
+	 */
+
+	private int calculatePointsForFullHouse(Dice[] dices) {
+		boolean foundThreeOfARow = false;
+		boolean foundTwoOfARow = false;
+		int indexOfDiceValueInThreeRow = 0;
+
+		int counter = 1, i = 0;
+
+		// first check if there are three of one number in a row
+		while (!foundThreeOfARow && i < dices.length) {
+
+			for (int j = i + 1; j <= dices.length - 1; j++) {
+				if (dices[i].getValue() == dices[j].getValue()) {
+					counter++;
+				}
+				if (counter == 3) {
+					// save index of dice because its value appears three times
+					indexOfDiceValueInThreeRow = i;
+					foundThreeOfARow = true;
+				}
+			}
+			counter = 1;
+			i++;
+		}
+
+		// checks if there are two of another number in a row
+		if (foundThreeOfARow) {
+
+			i = 0;
+			counter = 1;
+			while (!foundTwoOfARow && i < dices.length) {
+
+				for (int j = i + 1; j <= dices.length - 1
+						&& dices[i].getValue() != dices[indexOfDiceValueInThreeRow].getValue(); j++) {
+					if (dices[i].getValue() == dices[j].getValue()) {
+						counter++;
+					}
+
+					if (counter == 2) {
+						foundTwoOfARow = true;
+					}
+				}
+				counter = 1;
+				i++;
+			}
+
+		}
+
+		if (foundTwoOfARow && foundThreeOfARow) {
+			return 25;
+		} else {
+			return 0;
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param dices
+	 * @param nInARow
+	 * @return
+	 */
+	
+	private int calculatePointsForStraight(Dice[] dices, int nInARow) {
+		int [] v1 = {1,2,3,4};
+		int [] v2 = {2,3,4,5};
+		int [] v3 = {3,4,5,6};
+		int counter = 1, i = 0;
+		boolean conditionSucceed = false;
+		
+		while(!conditionSucceed && i < dices.length)
+			
+		for(int j = 0; j < v1.length; j++) {
+			if(dices[i].getValue() == v1[j]) {
+				counter++;
+			}
+			
+			if(counter == nInARow) {
+				conditionSucceed = true;
+			}
+		}
+		counter = 1;
+		i++;
+		
+		
+		
+		}
+		
+	}
+
+	// calculates and returns points for SmalLStraight and LargeStraight
+/*	private int calculatePointsForStraight(Dice[] dices, int nInARow) {
+		int points = (nInARow - 1) * 10;
+		int start = 1;
+		boolean straight = false;
+		while (!straight || start == 7 - nInARow) {
+			for (int i = 0; i < dices.length; i++) {
+				if (dices[i].getValue() == start)
+					start++;
+			}
+			if (start >= nInARow)
+				straight = true;
+		}
+		if (!straight)
+			return 0;
+		else
+			return points;
+	}
+
+	// calculates and returns points for Chance
+	private int calculatePointsForChance(Dice[] dices) {
+		int result = 0;
+		for (int i = 0; i < dices.length; i++) {
+			result += dices[i].getValue();
+		}
+		return result;
+	}
 }
+*/
--- src/tests/CalculatePointsTest.java
+++ src/tests/CalculatePointsTest.java
@@ -16,7 +16,7 @@ public class CalculatePointsTest {
 	private Dice[] dices = new Dice[5];
 	private Field f;
 
-	public  void setDicesValue(int[] values) {
+	public void setDicesValue(int[] values) {
 		for (int i = 0; i < dices.length; i++) {
 			dices[i] = new Dice(values[i]);
 		}
@@ -36,37 +36,37 @@ public class CalculatePointsTest {
 		setDicesValue(v1);
 		f.updatePoints(dices);
 		assertEquals(5, f.getPoints());
-	
+
 		f.resetPoints();
-		
+
 		int[] v2 = { 1, 1, 1, 1, 3 };
 		setDicesValue(v2);
 		f.updatePoints(dices);
 		assertEquals(4, f.getPoints());
-		
+
 		f.resetPoints();
-		
+
 		int[] v3 = { 1, 1, 5, 1, 3 };
 		setDicesValue(v3);
 		f.updatePoints(dices);
 		assertEquals(3, f.getPoints());
 
 		f.resetPoints();
-		
+
 		int[] v4 = { 6, 1, 5, 1, 3 };
 		setDicesValue(v4);
 		f.updatePoints(dices);
 		assertEquals(2, f.getPoints());
 
 		f.resetPoints();
-		
+
 		int[] v5 = { 6, 4, 5, 1, 3 };
 		setDicesValue(v5);
 		f.updatePoints(dices);
 		assertEquals(1, f.getPoints());
 
 		f.resetPoints();
-		
+
 		int[] v6 = { 6, 4, 5, 2, 3 };
 		setDicesValue(v6);
 		f.updatePoints(dices);
@@ -84,28 +84,34 @@ public class CalculatePointsTest {
 		assertEquals(10, f.getPoints());
 
 		f.resetPoints();
-		
-		int[] v2 = { 1, 2, 2, 2, 3 };
+
+		int[] v2 = { 1, 2, 2, 2, 2 };
 		setDicesValue(v2);
 		f.updatePoints(dices);
 		assertEquals(8, f.getPoints());
 
 		f.resetPoints();
-		
+
 		int[] v3 = { 2, 2, 5, 2, 3 };
 		setDicesValue(v3);
 		f.updatePoints(dices);
 		assertEquals(6, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v4 = { 6, 2, 5, 2, 3 };
 		setDicesValue(v4);
 		f.updatePoints(dices);
 		assertEquals(4, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v5 = { 6, 4, 5, 2, 3 };
 		setDicesValue(v5);
 		f.updatePoints(dices);
 		assertEquals(2, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v6 = { 6, 4, 5, 1, 3 };
 		setDicesValue(v6);
@@ -122,25 +128,35 @@ public class CalculatePointsTest {
 		f.updatePoints(dices);
 		assertEquals(15, f.getPoints());
 
-		int[] v2 = { 1, 3, 3, 3, 2 };
+		f.resetPoints();
+
+		int[] v2 = { 1, 3, 3, 3, 3 };
 		setDicesValue(v2);
 		f.updatePoints(dices);
 		assertEquals(12, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v3 = { 3, 3, 5, 3, 2 };
 		setDicesValue(v3);
 		f.updatePoints(dices);
 		assertEquals(9, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v4 = { 6, 3, 5, 3, 2 };
 		setDicesValue(v4);
 		f.updatePoints(dices);
 		assertEquals(6, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v5 = { 6, 4, 5, 3, 2 };
 		setDicesValue(v5);
 		f.updatePoints(dices);
 		assertEquals(3, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v6 = { 6, 4, 5, 1, 2 };
 		setDicesValue(v6);
@@ -157,25 +173,35 @@ public class CalculatePointsTest {
 		f.updatePoints(dices);
 		assertEquals(20, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v2 = { 4, 4, 4, 5, 4 };
 		setDicesValue(v2);
 		f.updatePoints(dices);
 		assertEquals(16, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v3 = { 1, 4, 4, 5, 4 };
 		setDicesValue(v3);
 		f.updatePoints(dices);
 		assertEquals(12, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v4 = { 1, 2, 4, 6, 4 };
 		setDicesValue(v4);
 		f.updatePoints(dices);
 		assertEquals(8, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v5 = { 2, 3, 4, 5, 6 };
 		setDicesValue(v5);
 		f.updatePoints(dices);
 		assertEquals(4, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v6 = { 1, 3, 2, 5, 6 };
 		setDicesValue(v6);
@@ -193,25 +219,35 @@ public class CalculatePointsTest {
 		f.updatePoints(dices);
 		assertEquals(25, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v2 = { 1, 5, 5, 5, 5 };
 		setDicesValue(v2);
 		f.updatePoints(dices);
 		assertEquals(20, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v3 = { 1, 3, 5, 5, 5 };
 		setDicesValue(v3);
 		f.updatePoints(dices);
 		assertEquals(15, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v4 = { 1, 2, 4, 5, 5 };
 		setDicesValue(v4);
 		f.updatePoints(dices);
 		assertEquals(10, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v5 = { 2, 6, 1, 3, 5 };
 		setDicesValue(v5);
 		f.updatePoints(dices);
 		assertEquals(5, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v6 = { 1, 4, 3, 6, 2 };
 		setDicesValue(v6);
@@ -229,25 +265,35 @@ public class CalculatePointsTest {
 		f.updatePoints(dices);
 		assertEquals(30, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v2 = { 1, 6, 6, 6, 6 };
 		setDicesValue(v2);
 		f.updatePoints(dices);
 		assertEquals(24, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v3 = { 3, 5, 6, 6, 6 };
 		setDicesValue(v3);
 		f.updatePoints(dices);
 		assertEquals(18, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v4 = { 1, 2, 6, 5, 6 };
 		setDicesValue(v4);
 		f.updatePoints(dices);
 		assertEquals(12, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v5 = { 1, 2, 4, 6, 5 };
 		setDicesValue(v5);
 		f.updatePoints(dices);
 		assertEquals(6, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v6 = { 1, 2, 3, 5, 4 };
 		setDicesValue(v6);
@@ -260,43 +306,72 @@ public class CalculatePointsTest {
 	public void testThreeOfOneKind() {
 		f = new Field(FieldType.ThreeOfOneKind);
 
+		
 		int[] v1 = { 1, 1, 1, 4, 5 };
 		setDicesValue(v1);
 		f.updatePoints(dices);
 		assertEquals(12, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v2 = { 2, 2, 2, 1, 5 };
 		setDicesValue(v2);
 		f.updatePoints(dices);
 		assertEquals(12, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v3 = { 3, 3, 3, 6, 2 };
 		setDicesValue(v3);
 		f.updatePoints(dices);
 		assertEquals(17, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v4 = { 1, 4, 4, 4, 5 };
 		setDicesValue(v4);
 		f.updatePoints(dices);
 		assertEquals(18, f.getPoints());
 
-		int[] v5 = { 1, 2, 5, 5, 5 };
+		f.resetPoints();
+
+		int[] v5 = { 1, 5, 2, 5, 5 };
 		setDicesValue(v5);
 		f.updatePoints(dices);
 		assertEquals(18, f.getPoints());
 
-		int[] v6 = { 6, 6, 6, 3, 1 };
+		f.resetPoints();
+
+		int[] v6 = { 6, 1, 6, 3, 6 };
 		setDicesValue(v6);
 		f.updatePoints(dices);
 		assertEquals(22, f.getPoints());
 
-		int[] v7 = { 1, 1, 3, 4, 5 };
+		f.resetPoints();
+		
+		int [] v7 = {1,1,1,1,5};
 		setDicesValue(v7);
+		f.updatePoints(dices);
+		assertEquals(9, f.getPoints());
+
+		f.resetPoints();
+		
+		int [] v8 = {1,1,1,1,1};
+		setDicesValue(v8);
+		f.updatePoints(dices);
+		assertEquals(5, f.getPoints());
+		
+		f.resetPoints();
+		
+		int[] v9 = { 1, 1, 3, 4, 5 };
+		setDicesValue(v9);
 		f.updatePoints(dices);
 		assertEquals(0, f.getPoints());
 
-		int[] v8 = { 1, 2, 3, 4, 5 };
-		setDicesValue(v8);
+		f.resetPoints();
+
+		int[] v10 = { 1, 2, 3, 4, 5 };
+		setDicesValue(v10);
 		f.updatePoints(dices);
 		assertEquals(0, f.getPoints());
 
@@ -311,40 +386,57 @@ public class CalculatePointsTest {
 		f.updatePoints(dices);
 		assertEquals(9, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v2 = { 2, 2, 2, 2, 1 };
 		setDicesValue(v2);
 		f.updatePoints(dices);
 		assertEquals(9, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v3 = { 3, 3, 3, 3, 2 };
 		setDicesValue(v3);
 		f.updatePoints(dices);
 		assertEquals(14, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v4 = { 4, 4, 4, 4, 5 };
 		setDicesValue(v4);
 		f.updatePoints(dices);
 		assertEquals(21, f.getPoints());
+		
+		f.resetPoints();
+
 
 		int[] v5 = { 1, 5, 5, 5, 5 };
 		setDicesValue(v5);
 		f.updatePoints(dices);
 		assertEquals(21, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v6 = { 6, 6, 6, 6, 2 };
 		setDicesValue(v6);
 		f.updatePoints(dices);
 		assertEquals(26, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v7 = { 1, 1, 1, 4, 5 };
 		setDicesValue(v7);
 		f.updatePoints(dices);
 		assertEquals(0, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v8 = { 1, 1, 3, 4, 5 };
 		setDicesValue(v8);
 		f.updatePoints(dices);
 		assertEquals(0, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v9 = { 1, 2, 3, 4, 5 };
 		setDicesValue(v9);
@@ -362,31 +454,41 @@ public class CalculatePointsTest {
 		f.updatePoints(dices);
 		assertEquals(25, f.getPoints());
 
+		f.resetPoints();
+		
 		int[] v2 = { 2, 2, 3, 3, 3 };
 		setDicesValue(v2);
 		f.updatePoints(dices);
 		assertEquals(25, f.getPoints());
 
+		f.resetPoints();
+		
 		int[] v3 = { 1, 1, 2, 3, 3 };
 		setDicesValue(v3);
 		f.updatePoints(dices);
 		assertEquals(0, f.getPoints());
-				
+
+		f.resetPoints();
+		
 		int[] v4 = { 1, 1, 1, 2, 3 };
 		setDicesValue(v4);
 		f.updatePoints(dices);
 		assertEquals(0, f.getPoints());
+
+		f.resetPoints();
 		
 		int[] v5 = { 1, 1, 1, 1, 3 };
 		setDicesValue(v5);
 		f.updatePoints(dices);
 		assertEquals(0, f.getPoints());
-	
+
+		f.resetPoints();
+		
 		int[] v6 = { 1, 1, 1, 1, 1 };
 		setDicesValue(v6);
 		f.updatePoints(dices);
 		assertEquals(0, f.getPoints());
-		
+
 	}
 
 	@Test
@@ -515,51 +617,70 @@ public class CalculatePointsTest {
 		f.updatePoints(dices);
 		assertEquals(50, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v2 = { 2, 2, 2, 2, 2 };
 		setDicesValue(v2);
 		f.updatePoints(dices);
 		assertEquals(50, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v3 = { 3, 3, 3, 3, 3 };
 		setDicesValue(v3);
 		f.updatePoints(dices);
 		assertEquals(50, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v4 = { 4, 4, 4, 4, 4 };
 		setDicesValue(v4);
 		f.updatePoints(dices);
 		assertEquals(50, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v5 = { 5, 5, 5, 5, 5 };
 		setDicesValue(v5);
 		f.updatePoints(dices);
 		assertEquals(50, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v6 = { 6, 6, 6, 6, 6 };
 		setDicesValue(v6);
 		f.updatePoints(dices);
 		assertEquals(50, f.getPoints());
 
-	
+		f.resetPoints();
+
 		int[] v7 = { 1, 1, 1, 1, 4 };
 		setDicesValue(v7);
 		f.updatePoints(dices);
 		assertEquals(0, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v8 = { 1, 1, 1, 2, 4 };
 		setDicesValue(v8);
 		f.updatePoints(dices);
 		assertEquals(0, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v9 = { 1, 1, 3, 2, 4 };
 		setDicesValue(v9);
 		f.updatePoints(dices);
 		assertEquals(0, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v10 = { 1, 5, 3, 2, 4 };
 		setDicesValue(v10);
 		f.updatePoints(dices);
 		assertEquals(0, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v11 = { 1, 2, 3, 5, 6 };
 		setDicesValue(v11);
@@ -577,30 +698,42 @@ public class CalculatePointsTest {
 		f.updatePoints(dices);
 		assertEquals(11, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v2 = { 1, 2, 3, 4, 6 };
 		setDicesValue(v2);
 		f.updatePoints(dices);
 		assertEquals(16, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v3 = { 5, 6, 3, 4, 1 };
 		setDicesValue(v3);
 		f.updatePoints(dices);
 		assertEquals(19, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v4 = { 1, 2, 3, 4, 5 };
 		setDicesValue(v4);
 		f.updatePoints(dices);
 		assertEquals(15, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v5 = { 6, 6, 5, 5, 5 };
 		setDicesValue(v5);
 		f.updatePoints(dices);
 		assertEquals(27, f.getPoints());
 
+		f.resetPoints();
+
 		int[] v6 = { 1, 2, 3, 3, 3 };
 		setDicesValue(v6);
 		f.updatePoints(dices);
 		assertEquals(12, f.getPoints());
+
+		f.resetPoints();
 
 		int[] v7 = { 1, 2, 2, 4, 1 };
 		setDicesValue(v7);
